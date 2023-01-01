312ec25e1504468aa7d98d7d16f45b55c95c0441
Zaki Indra <5442462-zaki-indra@users.noreply.gitlab.com>
2023-01-01 19:32:27 +0700

    Add support for parsing JSON file

--- discovery_index/graph-tool.py
+++ discovery_index/graph-tool.py
@@ -1,5 +1,7 @@
+import json
+import os
+from pathlib import Path
 import time
-import timeit
 from typing import Union
 import random
 from argparse import ArgumentParser
@@ -10,10 +12,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from yaspin import yaspin
 
 
 class VertexProperty:
-    #TODO: not implemented yet
+    def __init__(self, profile) -> None:
+        self.profile = profile
+
+
+class EdgeProperty:
+    #TODO: implement this
     def __init__(self) -> None:
         pass
 
@@ -41,7 +49,7 @@ class NeighborhoodVisitor(search.BFSVisitor):
         src_index = int(e.source())
         tgt_index = int(e.target())
 
-        # Don't let bfs examine all vertices
+        # Don't let bfs visit all vertices
         if self.hop[src_index] == self.hops:
             raise search.StopSearch()
 
@@ -49,19 +57,57 @@ class NeighborhoodVisitor(search.BFSVisitor):
         self.hop[tgt_index] = self.hop[src_index] + 1
         return super().tree_edge(e)
 
-    
+
 class DiscoveryGraph:
-    def __init__(self) -> None:
+    '''
+    Discovery Graph class
+    '''
+    def __init__(self, data_dir: str=".") -> None:
         self.graph = Graph(directed=False)
 
-    def add_node(self, column: dict):
+        self.graph.vertex_properties["profile"] = self.graph.new_vertex_property("python::object")
+        self.graph.edge_properties["profile"] = self.graph.new_edge_property("python::object")
+
+        self.parse_dir(data_dir)
+
+    def parse_dir(self, data_dir: Union[Path, str]):
+        '''
+        Parse whole directory of JSON file
+        '''
+        data_dir = Path(data_dir)
+        if not data_dir.is_dir():
+            #FIXME: Create appropriate exception
+            raise FileNotFoundError("The current path is not a directory")
+
+        for filename in os.listdir(data_dir):
+            filepath = os.path.join(data_dir, filename)
+            data = self.parse_json_file(filepath)
+
+            #FIXME: Refactor later
+            vertex_prop = VertexProperty(data)
+            self.add_vertex(vertex_prop)
+
+    def parse_json_file(self, file_path: Union[Path, str]):
+        '''
+        Parse JSON file
+        '''
+        if Path(file_path).suffix == ".json":
+            with open(file_path, 'r', encoding="utf-8") as file, \
+            yaspin(f"Reading {file_path} ...", color="white") as sp:
+                data = json.load(file)
+                sp.write(f"Finished reading {file_path}")
+            return data
+        else:
+            #FIXME: Create appropriate exception
+            raise FileNotFoundError(f"{file_path} is not a valid JSON file")
+
+    def add_vertex(self, profile: VertexProperty):
         '''
         Adds a single node, representing a column, into the nodes graph
         '''
 
         vertex = self.graph.add_vertex()
-        #TODO: Add vertex properties
-
+        self.graph.vertex_properties.profile[vertex] = profile
         return vertex
 
     def add_node_test(self, num_vertices:int=1) -> None:
@@ -70,6 +116,7 @@ class DiscoveryGraph:
         '''
 
         vertices = self.graph.add_vertex(num_vertices)
+        return vertices
 
     def add_edge(self, from_vertex, to_vertex):
         '''
@@ -106,7 +153,7 @@ class DiscoveryGraph:
             raise Exception()
         if hops == 0:
             return vertex
-        
+
         vertices = [vertex]
         hop = 1
         vertices.append(vertex.all_neighbors())
@@ -114,7 +161,7 @@ class DiscoveryGraph:
 
     def find_all_paths(self, from_vertex, to_vertex):
         return topology.all_paths(self.graph, self.graph.vertex(from_vertex), self.graph.vertex(to_vertex))
-        
+
 
 def test_graph(num_vertex, sparsity):
     '''
@@ -140,15 +187,17 @@ def test_graph(num_vertex, sparsity):
         graph_draw(graph, vertex_text=graph.vertex_index, output="graph.png")
 
     print(
-        f"Finding all paths between vertex 0 and 2...\n"\
+        f"Finding all paths between vertex 0 and 2\n"\
         f"Number of vertices: {num_vertex}\n"\
         f"Sparsity: {sparsity}"
     )
-    path_start = time.time()
-    paths = dg.find_all_paths(0, 2)
-    path_end = time.time()
 
-    print("DONE!")
+    with yaspin(text="Finding all paths...", color="white") as sp:
+        path_start = time.time()
+        paths = dg.find_all_paths(0, 2)
+        path_end = time.time()
+        sp.write("DONE!")
+
     print(f"Time elapsed {path_end-path_start} s")
     print("=============================================")
 
@@ -157,11 +206,13 @@ def test_graph(num_vertex, sparsity):
         f"Number of vertices: {num_vertex}\n"\
         f"Sparsity: {sparsity}"
     )
-    nbhd_start = time.time()
-    neighbors = dg.find_neighborhood(2, 2)
-    nbhd_end = time.time()
-    
-    print("DONE!")
+
+    with yaspin(text="Finding neighborhood...", color="white") as sp:
+        nbhd_start = time.time()
+        neighbors = dg.find_neighborhood(2, 2)
+        nbhd_end = time.time()
+        sp.write("DONE!")
+
     print(f"Time elapsed {nbhd_end-nbhd_start} s")
     print("=============================================")
 
@@ -170,7 +221,7 @@ def test_graph(num_vertex, sparsity):
 
 def test_scalability():
     '''
-    Testing the scalability of finding the 2-hop neighborhood of a node as well 
+    Testing the scalability of finding the 2-hop neighborhood of a node as well
     as finding paths between 2-nodes
     '''
     nodes = [100, 200, 400, 800, 1600, 3000, 5500, 8000, 10000, 12000, 14000]
@@ -185,21 +236,31 @@ def test_scalability():
             nbhd_result.append([n, s, nbhd_time])
             path_result.append([n, s, path_time])
 
-    for result, title in zip([nbhd_result, path_result], titles):
-        df = pd.DataFrame(result, columns = ['No. of Nodes', 'Sparsity', 'Time'])
-        df = df.pivot(index='No. of Nodes', columns='Sparsity', values='Time')
-        plt.figure()
-        df.plot(title=f'{title} Scalability')
-        plt.xticks(nodes, rotation=90)
-        plt.xlabel('No. of Nodes')
-        plt.ylabel('Time')
-        plt.savefig(f'{title}.png')
+    with yaspin(text="Generating plot...") as sp:
+        for result, title in zip([nbhd_result, path_result], titles):
+            df = pd.DataFrame(result, columns = ['No. of Nodes', 'Sparsity', 'Time'])
+            df = df.pivot(index='No. of Nodes', columns='Sparsity', values='Time')
+            plt.figure()
+            df.plot(title=f'{title} Scalability')
+            plt.xticks(nodes, rotation=90)
+            plt.xlabel('No. of Nodes')
+            plt.ylabel('Time')
+            plt.savefig(f'{title}.png')
+        sp.write("DONE!")
 
-def main():
-    test_scalability()
+def main(args):
+    print(args.path)
+    dg = DiscoveryGraph(args.path)
+
+    pass
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
+    parser = ArgumentParser(
+        prog = 'Network Builder',
+        description = 'Builds the Entreprise Knowledge Graph')
+    parser.add_argument("-p", "--path", help="Directory of JSON profile")
+    parser.add_argument("--benchmark", action="store_true")
     args = parser.parse_args()
-    main()
-
+    if args.benchmark:
+        test_scalability()
+    main(args)
